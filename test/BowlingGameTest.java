eb861a02779d6e835e97582e40972f2134cd3bcb
kkimbj18 <kkimbj18@ajou.ac.kr>
2021-04-05 21:34:19 +0900

    calculate score by for per frame temp

--- test/BowlingGameTest.java
+++ test/BowlingGameTest.java
@@ -32,4 +32,16 @@ public class BowlingGameTest {
 		assertEquals(20, g.score());
 	}
 	
+	@Ignore("until we walk frame by frame")
+	@Test
+	public void testOneSpare() {
+		g.roll(5);
+		g.roll(5);  //spare
+		g.roll(3);
+		
+		rollMany(17, 0);
+		
+		assertEquals(16, g.score());
+	}
+	
 }
